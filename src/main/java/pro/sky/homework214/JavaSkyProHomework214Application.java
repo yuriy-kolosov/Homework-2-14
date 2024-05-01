b2429147547b906b2448776fc25a46c93f30cbe3
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-05-01 22:42:54 +0300

    Домашнее задание 2-14. Spring и Mockito - начальный коммит

---
+++ src/main/java/pro/sky/homework214/JavaSkyProHomework214Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework214;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkyProHomework214Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSkyProHomework214Application.class, args);
+	}
+
+}
